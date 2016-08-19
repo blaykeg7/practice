0ef32b7ee1d38a4c9c5dad4b4ccd1a45df47ccef
Blayke Gasal <blayke7@hotmail.com>
2016-08-18 17:30:44 -0700

    renamed to index.html

--- scripts/blaykessports.js
+++ scripts/blaykessports.js
@@ -1,14 +1,15 @@
 
+var isMenuOpen = false;
+
+
+
 
 $(".dropdown").click(function(){
     //clear all show classes for all dropdowns
-    $(".dropdown").removeClass("show");
+    $(".dropdown").not($(this)).removeClass("show");
 
     //toggle show on the one that was clicked.
-   $(this).toggleClass ("show");
-
-   //adding sample code
-
-   //another sample code 2
+    
+$(this).toggleClass ("show");
 
 });
